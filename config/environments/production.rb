2b395668fc550c3a1ba397678c3f8f43988fa72e
Ali Amrah <ali.b5556789@gmail.com>
2019-11-17 07:56:12 +0300

    fix issue in producation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,15 +101,15 @@ Rails.application.configure do
   #  ===============
   config.action_mailer.perform_deliveries    = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options   = { host: Rails.application.credentials.host }
+  config.action_mailer.default_url_options   = { host:  'https://tutor-api.herokuapp.com' }
   config.action_mailer.delivery_method       = :smtp
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               "jisr.net",
-    user_name:            Rails.application.credentials.gmail[:username],
-    password:             Rails.application.credentials.gmail[:password],
+    user_name:            'ali_amrah',
+    password:             '77462600',
     authentication:       'plain',
     enable_starttls_auto: true
   }
