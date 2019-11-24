3869c1d177936caa03bcad8f5d52503739332d8d
Ali Amrah <ali.b5556789@gmail.com>
2019-11-24 22:24:45 +0300

    add authntication translation

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,6 @@
 class AuthenticationController < ApplicationController
   skip_before_action :authorize_request, only: :authenticate
+  before_action :set_locale
 
     # return auth token once user is authenticated
   def authenticate
@@ -8,12 +9,20 @@ class AuthenticationController < ApplicationController
     user = User.find_by(email: auth_params[:email])
     User.current = user
     data = { user: user.as_api_response(:show), auth_token: auth_token }
-    json_response(data: data)
+    json_response(
+      message: I18n.t(:login),
+      data: data
+    )
   end
 
   private
 
   def auth_params
     params.require(:user).permit(:email, :password)
+  end
+
+  # Set request locale
+  def set_locale
+    I18n.locale = params[:locale] || request.headers['locale'] || I18n.default_locale
   end
 end
--- app/lib/message.rb
+++ app/lib/message.rb
@@ -5,30 +5,22 @@ class Message
   end
 
   def self.invalid_credentials
-    'Invalid credentials'
+    I18n.t(:invalid_credentials)
   end
 
   def self.invalid_token
-    'Invalid token'
+    I18n.t(:invalid_token)
   end
 
   def self.missing_token
-    'Missing token'
+    I18n.t(:missing_token)
   end
 
   def self.unauthorized
-    'Unauthorized request'
-  end
-
-  def self.account_created
-    'Account created successfully'
-  end
-
-  def self.account_not_created
-    'Account could not be created'
+    I18n.t(:unauthorized)
   end
 
   def self.expired_token
-    'Sorry, your token has expired. Please login to continue.'
+    I18n.t(:expired_token)
   end
 end
