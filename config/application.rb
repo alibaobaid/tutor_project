4180b586209cf882e8f4637250b3e548770c0006
Ali Amrah <ali.b5556789@gmail.com>
2019-12-03 06:44:30 +0300

    fix issue in dashbord

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,8 @@ module TutorProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
